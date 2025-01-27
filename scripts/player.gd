95dd1fe062514658569ace322255f4ad6e0913a5
red23714 <redredovich0@gmail.com>
2025-01-28 01:02:23 +0300

    Add some visual effects

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,51 @@
 extends CharacterBody2D
 
-const SPEED = 260.0
+signal health_changed
+
+const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
+var acceleration: float = 1.0
 var deceleration: float = 1.0
+
+@export var jump_height: float = 100
+@export var jump_time_to_peak: float = 100
+@export var jump_time_to_descent: float = 100
+
+@onready var jump_velocity: float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
 var is_fly : bool = false
 
-var is_alive : bool = true
+var max_health : float = 3
+var health : float = max_health
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hook_controller: Node2D = $HookController
+@onready var sword: Area2D = $Sword
+
+func get_gravity_player() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+
+func take_damage(amount: float):
+	health -= amount
+	
+	animated_sprite.modulate = Color.RED
+	await get_tree().create_timer(0.2).timeout
+	animated_sprite.modulate = Color.WHITE
+	
+	health_changed.emit()
+	
+	if health <= 0:
+		GameManager.reload_scene()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += get_gravity_player() * delta
 	
-	if is_alive:
+	if health > 0:
 		# Handle jump.
 		if Input.is_action_just_pressed("jump") && (is_on_floor() || hook_controller.launched):
 			velocity.y += JUMP_VELOCITY
@@ -26,19 +54,27 @@ func _physics_process(delta: float) -> void:
 		# Flip sprite
 		if velocity.x > 0:
 			animated_sprite.flip_h = false
+			sword.attack_animation = "attack"
 		elif velocity.x < 0:
 			animated_sprite.flip_h = true
-		
-		# Play animations
-		if is_on_floor():
-			animated_sprite.play("idle")
-		elif !hook_controller.launched:
-			animated_sprite.play("jump")
+			sword.attack_animation = "attack_left"
 		
 		# Get input direction: -1, 0, 1
 		var direction := Input.get_axis("left", "right")
+		
+		# Play animations
+		if is_on_floor():
+			if direction != 0:
+				animated_sprite.play("run")
+			else:
+				animated_sprite.play("idle")
+		elif hook_controller.launched:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("jump")
+		
 		if direction:
-			velocity.x = direction * SPEED * int(is_fly)
+			velocity.x = lerp(velocity.x, direction * SPEED * (1 + int(is_fly)), acceleration)
 		else:
 			velocity.x = lerp(velocity.x, 0.0, deceleration)
 		
---
+++ scripts/sword.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var attack_animation : String = "attack"
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack"):
+		animation_player.play(attack_animation)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Enemy"):
+		if owner.get_node("HookController").target_obj == area:
+			area.take_damage(100)
+		else:
+			area.take_damage(1)
+		owner.get_node("HookController").retract()
